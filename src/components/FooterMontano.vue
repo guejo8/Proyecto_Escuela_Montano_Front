19d2ccb1b9aebc3cb984c2e13563673ecc9233d8
ikatza1967 <ikatzacarbon1@gmail.com>
2023-07-17 21:04:36 +0200

    footer

--- src/components/FooterMontano.vue
+++ src/components/FooterMontano.vue
@@ -1,50 +1,55 @@
-<template>
-  <!-- Site footer -->
-  <footer class="site-footer bg-">
-    <div class="container  ">
+ <template> 
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- -----------------Site footer ---------------- -->
+   <footer class="site-footer bg-">
+    <div class="container-fluid  ">
       <div class="row">
-        <div class="col-sm-12 col-md-4 p-lg-3 ">
+        <div class="col-sm-12 col-md-5 p-lg-3 ">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.065870029503!2d-2.9212682602773956!3d43.270995823811774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e4fc1a40c4bcb%3A0x605fd1698ff98ecc!2sMonta%C3%B1o%20Zentroa!5e0!3m2!1ses!2ses!4v1689152373737!5m2!1ses!2ses"
-            width="250"
-            height="150"
+            width="280"
+            height="290"
             style="border: 0"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
 
-          <h6></h6>
+          <h6></h6> 
           <!-- <p class="text-justify"><i>BiciEnergy</i> busca hacer uso de bicicletas para generar energía y
                     utilizarla en la ciudad es una iniciativa interesante que combina la actividad física con la
                     generación de electricidad. Este concepto se conoce comúnmente como "bicicletas generadoras". -->
-        </div>
+         </div>
 
-        <div class="col-xs-6 col-md-3  d-flex  p-lg-3"> 
+        <div class="col-xs-6 col-md-3   p-lg-3"> 
             <h3>Como llegar:</h3>
             
-            <img src="" alt="Bilbobus: A1">
-            <img src="" alt="Metro linea 3:">
-            <i class="fa-solid fa-bus-simple"></i>
+            
+            <i class="fa-solid fa-bus-simple">  Bilbobus:A1</i>
+            <br/>
+            <br/>
+              
+            <i class="fa-solid fa-bus-simple"> Metro linea 3</i>
+            
         </div>
 
-        <div class="col-xs-3 col-md-3 d-flex p-lg-3 ">
-          <!-- <h6>Secciones</h6> -->
-          <!-- <ul class="footer-links"> -->
-              <img class="recycling" src="../assets/recycling.png" alt="" />
-            <div>
-              <p style="font-size: 18px; font-weight: bold;font-family: 'Open' sans-serif;"> Nuestros vasos,platos y cubiertos son biodegradables,compostables:hecchos a base de fecula de maiz </p>
+        <div class="col-xs-3 col-md-4 d-flex p-lg-4 ">
+          
+          
+          <div> 
+              <img class="recycling" src="../assets/recyclinglogo.png" alt="" />
+              <p style="font-size: 18px; font-weight: bold;margin: 3em;: ; font-family: 'Open' sans-serif;"> Nuestros vasos,platos y cubiertos son biodegradables,compostables:hecchos a base de fecula de maiz </p>
              <p style="font-size: 12px; font-weight: bold;font-family: 'Oswald', sans-serif; " >Empresa comprometida con el medio ambiente:Peñascal Scoop.</p> 
-            </div>
+             </div> 
         </div>
       </div>
       <hr />
-    </div>
-                  <!-- ------------copyright centrado------------- -->
+    </div> 
+                  <!-- ------------copyright centrado-------------  -->
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-sm-2 col-xs-6  d-flex justify-content-end">
-          <img class="logot" src="../assets/img/logtras.png"> 
+          <img class="logot" src="../assets/logotrasparent.png"> 
             <p class="copyright-text">
             Copyright &copy; 2023 All Rights Reserved by
              </p>
@@ -59,30 +64,16 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.mapouter {
-  position: relative;
-  text-align: right;
-  width: 100%;
-  height: 99px;
-}
 
-.gmap_canvas {
-  overflow: hidden;
-  background: none !important;
-  width: 100%;
-  height: 99px;
-  -radius: 40px;
-}
 
-.gmap_iframe {
-  height: 99px !important;
-}
+
 .recycling {
-  width: 40%;
-  margin-bottom: 5em;
+  width: 20%;
+  margin-bottom: 3em;
   display: block;
   margin-left: 40%;
-  padding-left: 10%;
+  padding-left: 0.5%;
+  border-radius: 50%;
 }
 .site-footer{
     background-color: #008948;
@@ -93,4 +84,15 @@
 .copyright-text{
     padding-top: 1.5em;
 }
-</style>
+
+</style> -->
+
+
+
+
+
+
+
+
+
+
