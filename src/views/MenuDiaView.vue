a23ab44f8b0d5e96aeba46d302ef4ce1b34c179a
jonathaneche <jonathan.eche@gmail.com>
2023-07-18 10:12:11 +0200

    creacion de vistas

---
+++ src/views/MenuDiaView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Vista menu del dia</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/PasteleriaView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Vista Pasteleria</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
