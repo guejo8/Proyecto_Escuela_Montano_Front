e96622c0cbef620650b4aa76be8b49e9df57853f
Medibra322 <medibra322@gmail.com>
2023-07-18 13:30:20 +0200

    Componente Tarjeta montano dividida en dos, ya las imagenes esta enrutadas

--- src/components/HomeMontano.vue
+++ src/components/HomeMontano.vue
@@ -1,9 +1,63 @@
 <template>
-    <div class="imagenHome">
-        <h2>OSTALARITZA ESKOLA</h2>
+    <div class="background-container">
+      <div class="line-overlay"></div>
+      <h2 class="title1">OSTALARITZA ESKOLA</h2>
     </div>
-</template>
+  </template>
+  
+  <script>
 
-<script>
-
-</script>
+  </script>
+  
+  <style>
+  .background-container {
+    background-image: url('../assets/img/Img_Home/foto_yerson.png');
+    background-size: cover;
+    padding: 5rem;
+    min-height: 70vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .line-overlay {
+    position: absolute;
+    top: 27rem;
+    left: 6rem;
+    width: 90%;
+    height: 0.6rem;
+    background-color: #0D904F;
+  }
+  
+  .title1 {
+    color: #FFFFFF;
+    text-align: center;
+    padding-top: 17rem;
+    font-size: 5rem;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  
+  
+  
+  
+  /* Media Query para pantallas pequeñas (ejemplo: teléfonos móviles) */
+  @media (max-width: 576px) {
+    .background-container {
+      padding: 3rem;
+      min-height: 50vh;
+    }
+  
+    .title1 {
+      font-size: 3rem;
+      padding-top: 10rem;
+    }
+  
+    .line-overlay {
+      top: 17rem; /* Ajusta la posición de la línea para pantallas pequeñas */
+      left: 1rem;
+    }
+  
+  }
+  
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template>
-
+<HomeMontano/>
 <TarjetasMontano/>
 
   
   </template>
   
   <script  setup>
+  import HomeMontano from "../components/HomeMontano.vue"
   import TarjetasMontano from "../components/TarjetasMontano.vue"
 
  
