5223c3815dfa3bf399aa8dbe1b95b42d0e7e87d6
jonathaneche <jonathan.eche@gmail.com>
2023-07-20 17:58:22 +0200

    Creacion de layouts para Frnt e intranet (Dashboard)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
-  <NavbarMontanoIdiomas/>
   <router-view/>
-  <FooterMontano/>
 </template>
 
 <script  setup>
-import NavbarMontanoIdiomas from '@/components/NavbarMontanoIdiomas.vue';
-import FooterMontano from './components/FooterMontano.vue';
 
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,75 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import AppLayout from "../layouts/AppLayout.vue";
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    component: AppLayout,
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () => import("../views/HomeView.vue"),
+      },
+      {
+        path: "/about",
+        name: "about",
+        component: () => import("../views/AboutView.vue"),
+      },
+      {
+        path: "/contacto",
+        name: "contacto",
+        component: () => import("../views/ContactoView.vue"),
+      },
+      {
+        path: "/menu-dia",
+        name: "menu-dia",
+        component: () => import("../views/MenuDiaView.vue"),
+      },
+      {
+        path: "/menu-estrella",
+        name: "menu-estrella",
+        component: () => import("../views/MenuEstrellaView.vue"),
+      },
+      {
+        path: "/carniceria",
+        name: "carniceria",
+        component: () => import("../views/CarniceriaView.vue"),
+      },
+      {
+        path: "/pasteleria",
+        name: "pasteleria",
+        component: () => import("../views/PasteleriaView.vue"),
+      },
+      {
+        path: "/raciones",
+        name: "raciones",
+        component: () => import("../views/RacionesView.vue"),
+      },
+      {
+        path: "/pintxos",
+        name: "pintxos",
+        component: () => import("../views/PintxosView.vue"),
+      },
+    ],
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
-  },
-  {
-    path: "/contacto",
-    name: "contacto",
-    component: () => import("../views/ContactoView.vue"),
-  },
-  {
-    path: "/menu-dia",
-    name: "menu-dia",
-    component: () => import("../views/MenuDiaView.vue"),
-  },
-  {
-    path: "/menu-estrella",
-    name: "menu-estrella",
-    component: () => import("../views/MenuEstrellaView.vue"),
-  },
-  {
-    path: "/carniceria",
-    name: "carniceria",
-    component: () => import("../views/CarniceriaView.vue"),
-  },
-  {
-    path: "/pasteleria",
-    name: "pasteleria",
-    component: () => import("../views/PasteleriaView.vue"),
-  },
-  {
-    path: "/raciones",
-    name: "raciones",
-    component: () => import("../views/RacionesView.vue"),
-  },
-  {
-    path: "/pintxos",
-    name: "pintxos",
-    component: () => import("../views/PintxosView.vue"),
+    path: "/dashboard",
+    name: "dashboard",
+    component: DashboardLayout, // Use the DashboardLayout for the dashboard routes
+    children: [
+      {
+        path: "",
+        name: "principal",
+        component: () => import("../views/DashboardView.vue"),
+      },
+      {
+        path: "/dashboard2",
+        name: "dashboard2",
+        component: () => import("../views/DashboardView2.vue"),
+      },
+    ],
   },
 ];
 
