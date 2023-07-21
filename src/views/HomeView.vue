8a3f0528e9a0762e4dbe738070441c86efc5902b
Trontzal <trontzal@gmail.com>
2023-07-21 10:24:32 +0200

    he quitado el compoinente adminitradoe del gome

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-  <AdministrarProductos />
   <HomeMontano/>
   <TutorialSiempreCerca/>
   <TarjetasMontano/>
@@ -10,8 +9,4 @@
 import TutorialSiempreCerca from "@/components/TutorialSiempreCerca.vue";
 import HomeMontano from "../components/HomeMontano.vue"
 import TarjetasMontano from "../components/TarjetasMontano.vue"
-import AdministrarProductos from "@/components/AdministrarProductos.vue";
-
- 
-
 </script>
